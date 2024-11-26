2701c35360d6f578ff374ef507e272e3900ad2c3
Moh <mmahmud@wearebarbarian.com>
2024-11-25 19:43:12 -0500

    add button toggle changing inner text

--- src/modules/pricing-card.module/module.js
+++ src/modules/pricing-card.module/module.js
@@ -26,8 +26,17 @@ function addButtonClickListener() {
   const buttons = document.querySelectorAll('.moh-card_price-button');
   buttons.forEach(button => {
     button.addEventListener("click", () => {
-      buttons.forEach(btn => btn.classList.remove("active"));
-      button.classList.add("active");
+      if (button.classList.contains("active")) {
+        button.classList.remove("active");
+        button.innerHTML = 'Request product';
+      } else {
+        buttons.forEach(btn => {
+          btn.classList.remove("active");
+          btn.innerHTML = 'Request product';
+        });
+        button.classList.add("active");
+        button.innerHTML = '<span>&#10003;</span> Product requested';
+      }
     });
   });
 }
@@ -35,5 +44,3 @@ function addButtonClickListener() {
 window.addEventListener('load', equalizeCardHeights);
 window.addEventListener('resize', equalizeCardHeights);
 window.addEventListener('DOMContentLoaded', addButtonClickListener);
-
-//
